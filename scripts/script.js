ce32fce2a2a13f4e7b878f9bc0bd96aacc826191
Matthew Wester <MatthewWester@live.com>
2020-01-02 22:34:13 -0700

    Site is like me now, looks ugly but works well, gives 5 day forecast

--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,67 @@
 
+//below function saves a list of searched cities in localStorage
 function saveCity() {
     var city = $("#cityInput").val();
     pastSavedCities = localStorage.getItem("savedCities");
     if(pastSavedCities == null) {
-        console.log("It was null!");
         localStorage.setItem("savedCities", city);
     } else {
     newSavedCities = pastSavedCities + ", " + city;
-    localStorage.setItem("savedCities", newSavedCities);
-    console.log(newSavedCities);}
+    localStorage.setItem("savedCities", newSavedCities);}
     $("#citiesSearched").empty();
     $("#citiesSearched").append("<p>Past searches: " + localStorage.getItem("savedCities") + "</p>");
 }
 
+//below function gets weather info for searched city
 function citySearchButton() {
     var city = $("#cityInput").val();
     var lat = 0;
     var lon = 0;
-    console.log(city);
     $.ajax({
-        // url: "api.openweathermap.org/data/2.5/weather?q=" + city + ",us&mode=json&appid=52f8ba8465b83eb0e9c8d13385d0ea14",
         url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&units=imperial&appid=52f8ba8465b83eb0e9c8d13385d0ea14",
         method: "GET"
     }) .then(function(response) {
-        $("#currentCityWeather").append("<p>City's name: " + response.name + "</p>")
+        $("#currentCityWeather").append("<p>" + response.name + "</p>")
         .append("<p>Date: " + (new Date(response.dt * 1000).toUTCString()) + "</p>")
         .append("<p>" + response.weather[0].description + " " + "<img src=http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png alt='Weather Icon Here'></p>")
         .append("<p>Temperature: " + response.main.temp + " degrees Fahrenheit</p>")
         .append("<p>Humidity: " + response.main.humidity + "%</p>")
         .append("<p>Wind Speed: " + response.wind.speed + " mph</p>");
-        console.log(response);
         lon = response.coord.lon;
         lat = response.coord.lat;
-        console.log(lon);
-        console.log(lat);
         $.ajax({
             url: "http://api.openweathermap.org/data/2.5/uvi?appid=52f8ba8465b83eb0e9c8d13385d0ea14&lat=" + lat + "&lon=" + lon,
             method: "GET"
         }) .then(function(response) {
-            console.log(lon);
-            console.log(lat);
             $("#currentCityWeather").append("<p>UV Index: " + response.value + "</p>");
         })
     })
-
-
-//     $.ajax({
-//         // url: "https://www.api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&mode=json&appid=52f8ba8465b83eb0e9c8d13385d0ea14",
-//         url: "https://api.openweathermap.org/data/2.5/weather?q=" + "London" + ",uk&appid=52f8ba8465b83eb0e9c8d13385d0ea14",
-//         method: "GET"
-//     }) .then(function(response) {
-//         console.log(response);
-// $("card-text5").append("<p>" + JSON.stringify(response) + "</p>");
-//     })
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&units=imperial&appid=52f8ba8465b83eb0e9c8d13385d0ea14",
+        method: "GET"
+    }) .then(function(response) {
+        //Grabs the weather snapshot for noon of each day because every item in array is a 3 hour forecast
+        $("#day1").append("<p>" + (new Date(response.list[2].dt * 1000).toUTCString()) + "</p>")
+        .append("<img src=http://openweathermap.org/img/wn/" + response.list[2].weather[0].icon + "@2x.png alt='Weather Icon Here'>")
+        .append("<p>Temperature: " + response.list[2].main.temp + "</p>")
+        .append("<p>Humidity: " + response.list[2].main.humidity + "</p>");
+        $("#day2").append("<p>" + (new Date(response.list[10].dt * 1000).toUTCString()) + "</p>")
+        .append("<img src=http://openweathermap.org/img/wn/" + response.list[10].weather[0].icon + "@2x.png alt='Weather Icon Here'>")
+        .append("<p>Temperature: " + response.list[10].main.temp + "</p>")
+        .append("<p>Humidity: " + response.list[10].main.humidity + "</p>");
+        $("#day3").append("<p>" + (new Date(response.list[18].dt * 1000).toUTCString()) + "</p>")
+        .append("<img src=http://openweathermap.org/img/wn/" + response.list[18].weather[0].icon + "@2x.png alt='Weather Icon Here'>")
+        .append("<p>Temperature: " + response.list[18].main.temp + "</p>")
+        .append("<p>Humidity: " + response.list[18].main.humidity + "</p>");
+        $("#day4").append("<p>" + (new Date(response.list[26].dt * 1000).toUTCString()) + "</p>")
+        .append("<img src=http://openweathermap.org/img/wn/" + response.list[26].weather[0].icon + "@2x.png alt='Weather Icon Here'>")
+        .append("<p>Temperature: " + response.list[26].main.temp + "</p>")
+        .append("<p>Humidity: " + response.list[26].main.humidity + "</p>");
+        $("#day5").append("<p>" + (new Date(response.list[34].dt * 1000).toUTCString()) + "</p>")
+        .append("<img src=http://openweathermap.org/img/wn/" + response.list[34].weather[0].icon + "@2x.png alt='Weather Icon Here'>")
+        .append("<p>Temperature: " + response.list[34].main.temp + "</p>")
+        .append("<p>Humidity: " + response.list[34].main.humidity + "</p>");
+    });
 }
 
 
